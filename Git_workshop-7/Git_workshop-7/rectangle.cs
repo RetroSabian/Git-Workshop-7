a07eb4a577212a1381ccf916abd9db7f70628319
ThandiTQ <ttshabalala@retrorabbit.co.za>
2019-01-17 10:55:27 +0200

    Calculate rectangle area class

---
+++ Git_workshop-7/Git_workshop-7/rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_workshop_7
+{
+    class rectangle
+    {
+
+        public void Area()
+        {
+            double length = Console.Read();
+            double with = Console.Read();
+
+            double RecArea = length * width;
+
+        }
+
+    }
+}
