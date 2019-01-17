ad885d80b1d7b9748a56ebcc748db903474ecc42
RetroSabian <srajbully@retrorabbit.co.za>
2019-01-17 10:16:57 +0200

    Created project

---
+++ Git_workshop-7/Git_workshop-7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_workshop_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
