ae4de3c2fcfd38a67f1cb7c1a9dd729b19d89e3d
nnyanisa <nnyanisa@retrorabbit.co.za>
2019-01-17 11:01:18 +0200

    created square class

---
+++ Git_workshop-7/Git_workshop-7/square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_workshop_7
+{
+    class square
+    {
+        double side = Convert.ToDouble(Console.ReadLine());
+
+        double square()
+        {
+            return side * side;
+        }
+    }
+}
