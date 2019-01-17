9b5ddc8a4aef585cab5d4da071f1922c74e50564
Arsalan <amtechz.info@gmail.com>
2019-01-17 10:49:17 +0200

    added hexagon class

---
+++ Git_workshop-7/Git_workshop-7/hexagon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_workshop_7
+{
+    class hexagon
+    {
+        double side;
+
+        public hexagon(double s)
+        {
+            side = s;
+        }
+
+        public double Area()
+        {
+            return side * side * 3 / 2 * Math.Sqrt(3);
+        }
+
+    }
+}
