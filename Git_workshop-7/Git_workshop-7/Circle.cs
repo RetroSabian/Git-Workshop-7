ccf200d81d39a2c471cd94f99f426e15971873c8
flekhulani <flekhulani@retrorabbit.co.za>
2019-01-17 10:50:53 +0200

    Circle class

--- Git_workshop-7/Git_workshop-7/Circle.cs
+++ Git_workshop-7/Git_workshop-7/Circle.cs
@@ -5,7 +5,8 @@ namespace Git_workshop_7
     class Circle
     {
         public void Area(){
-            
+            double radius = Console.Read();
+            Console.WriteLine("The circle radius is "+Math.pi*radius*radius);
         }
     }
 }
